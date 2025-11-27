0034a86e2930b9885944ea91d9655a96c46de0d2
Rainy <tanchuonghuynh3@gmail.com>
2025-11-27 12:03:13 +0700

    Update scss App and export pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Intro from './pages/intro/Intro';
-import Login from './pages/login/Login';
-import Register from './pages/register/Register';
-import './App.css';
+// import Intro from './pages/intro/Intro';
+// import Login from './pages/login/Login';
+// import Register from './pages/register/Register';
+import './App.scss';
+import { Intro, Login, Register, Home } from './pages';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Intro />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
